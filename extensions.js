9397c47a029f193b3988b4a45a7f1412e0ce4913
Gregory Yeghiyan <46092559+RaspberryEqualsPi@users.noreply.github.com>
2022-09-26 21:28:22 -0400

    Create extensions.js

---
+++ extensions.js
@@ -0,0 +1,27 @@
+let str = window.location.href;
+let id = str.substring(str.length - str.split("").reverse().join("").indexOf("/"));
+function requestExtension(id, callback){
+    callback("installable");
+}
+chrome.webstorePrivate.requestExtension = requestExtension;
+fetch('https://apithingy.000webhostapp.com/get_manifest.php', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: "id=" + id,
+})
+  .then((response) => response.text())
+  .then((data) => {
+    var c = {
+        id: id,
+        manifest: data,
+        localizedName: "bypassed extension :)",
+        locale: "en-us"
+    };
+    chrome.webstorePrivate.beginInstallWithManifest3(c, alert);
+    chrome.webstorePrivate.completeInstall(id, alert);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
